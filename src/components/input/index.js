689837a2aaaacafdcc5fcdd930550367be0ab4d0
Jorge Rodriguez <jorge-11@live.com.ar>
2022-06-18 11:18:35 -0300

    switch clase 7 terminado

---
+++ src/components/input/index.js
@@ -0,0 +1,10 @@
+import react from "react";
+import { TextInput } from "react-native";
+import { styles } from "./styles";
+
+const Input = ({ style, ...props }) => 
+{
+  return <TextInput {...props} style={[styles.input, { ...style }]} />;
+};
+
+export default Input;
---
+++ src/constants/colors.js
@@ -0,0 +1,9 @@
+export default 
+{
+    primary: '#FFA69E',
+    secondary: '#5E6472',
+    textColorPrimary: '#000000',
+    textColorSecondary: '#ffffff',
+    white: '#ffffff',
+    shadow: '#000000',
+}
